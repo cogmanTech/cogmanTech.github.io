12bafc492591496093d9c4511e35f468445b5e1f
Van Noel <vannoel0628@gmail.com>
2023-12-14 12:31:18 +0800

    renew official website

--- _next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/zh":["static\u002Fchunks\u002Fpages\u002Fzh.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fzh"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
